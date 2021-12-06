681dec4986cf84b42e4db5111b729a454603cf4a
fauzanzulilkram <95626939+fauzanzulilkram@users.noreply.github.com>
2021-12-06 20:27:46 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -39,7 +39,7 @@ async function classify(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/ercooss/bot_telegram/main/public/cls_model/model.json';
+        const path = 'https://raw.githubusercontent.com/fauzanzulilkram/fauzanbot/main/public/cls_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
