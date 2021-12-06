bf422b1eb89e0f3472280143f4db67afd2534216
fauzanzulilkram <95626939+fauzanzulilkram@users.noreply.github.com>
2021-12-06 20:26:43 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -23,7 +23,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/ercooss/bot_telegram/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/fauzanzulilkram/fauzanbot/main/public/ex_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
